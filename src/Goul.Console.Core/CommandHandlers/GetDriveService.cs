3727828bdc07ad73920fa1a1502052ab8b203527
Andy Sipe <morgan.talk@gmail.com>
2013-07-25 14:56:00 -0400

    Separated the drive service initialization of the uploader into it's own object

---
+++ src/Goul.Console.Core/CommandHandlers/GetDriveService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DotNetOpenAuth.OAuth2;
+using Google.Apis.Authentication.OAuth2;
+using Google.Apis.Authentication.OAuth2.DotNetOpenAuth;
+using Google.Apis.Drive.v2;
+using Google.Apis.Services;
+using Goul.Console.Core.Storage;
+using Goul.Core;
+using SupaCharge.Core.IOAbstractions;
+
+namespace Goul.Console.Core.CommandHandlers {
+  public static class GetDriveService {
+    public static DriveService GetService() {
+      var provider = Constants.GetAppClient(new Setup().GetCredentialsRepository().Load());
+      var auth = new OAuth2Authenticator<NativeApplicationClient>(provider, GetAuthorization);
+      var service = new DriveService(new BaseClientService.Initializer {
+        Authenticator = auth
+      });
+
+      return service;
+    }
+
+    private static IAuthorizationState GetAuthorization(NativeApplicationClient appClient) {
+      var tokenRepository = new RefreshTokenRepository(new DotNetFile(), "refreshToken.txt");
+      var code = tokenRepository.Load()[0];
+
+      var state = new AuthorizationState(
+        Constants.GetScopes()) { Callback = new Uri(NativeApplicationClient.OutOfBandCallbackUrl), RefreshToken = code };
+      appClient.RefreshToken(state);
+      return state;
+    }
+  }
+}
--- src/Goul.Console.Core/CommandHandlers/UploaderHandler.cs
+++ src/Goul.Console.Core/CommandHandlers/UploaderHandler.cs
@@ -15,11 +15,13 @@ using File = Google.Apis.Drive.v2.Data.File;
 namespace Goul.Console.Core.CommandHandlers {
   public class UploaderHandler : ICommandHandler {
     public void Execute(params string[] args) {
-      var provider = Constants.GetAppClient(new Setup().GetCredentialsRepository().Load());
-      var auth = new OAuth2Authenticator<NativeApplicationClient>(provider, GetAuthorization);
-      var service = new DriveService(new BaseClientService.Initializer {
-        Authenticator = auth
-      });
+      /* var provider = Constants.GetAppClient(new Setup().GetCredentialsRepository().Load());
+       var auth = new OAuth2Authenticator<NativeApplicationClient>(provider, GetAuthorization);
+       var service = new DriveService(new BaseClientService.Initializer {
+         Authenticator = auth
+       });*/
+
+      var service = GetDriveService.GetService();
 
       var body = new File {Title = args[1], Description = "A test document"};
       var stream = new MemoryStream(System.IO.File.ReadAllBytes(args[0]));
@@ -42,7 +44,7 @@ namespace Goul.Console.Core.CommandHandlers {
       return contentType;
     }
 
-    private static IAuthorizationState GetAuthorization(NativeApplicationClient appClient) {
+ /*   private static IAuthorizationState GetAuthorization(NativeApplicationClient appClient) {
       var tokenRepository = new RefreshTokenRepository(new DotNetFile(), "refreshToken.txt");
       var code = tokenRepository.Load()[0];
 
@@ -51,7 +53,7 @@ namespace Goul.Console.Core.CommandHandlers {
       {Callback = new Uri(NativeApplicationClient.OutOfBandCallbackUrl), RefreshToken = code};
       appClient.RefreshToken(state);
       return state;
-    }
+    }*/
   }
 }
 
