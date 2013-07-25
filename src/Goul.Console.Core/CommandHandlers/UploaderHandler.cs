15ab266570bf1f58384320bc564e2b4ec677d87f
Andy Sipe <morgan.talk@gmail.com>
2013-07-25 14:57:03 -0400

    removed comments

--- src/Goul.Console.Core/CommandHandlers/UploaderHandler.cs
+++ src/Goul.Console.Core/CommandHandlers/UploaderHandler.cs
@@ -15,12 +15,6 @@ using File = Google.Apis.Drive.v2.Data.File;
 namespace Goul.Console.Core.CommandHandlers {
   public class UploaderHandler : ICommandHandler {
     public void Execute(params string[] args) {
-      /* var provider = Constants.GetAppClient(new Setup().GetCredentialsRepository().Load());
-       var auth = new OAuth2Authenticator<NativeApplicationClient>(provider, GetAuthorization);
-       var service = new DriveService(new BaseClientService.Initializer {
-         Authenticator = auth
-       });*/
-
       var service = GetDriveService.GetService();
 
       var body = new File {Title = args[1], Description = "A test document"};
@@ -43,17 +37,6 @@ namespace Goul.Console.Core.CommandHandlers {
 
       return contentType;
     }
-
- /*   private static IAuthorizationState GetAuthorization(NativeApplicationClient appClient) {
-      var tokenRepository = new RefreshTokenRepository(new DotNetFile(), "refreshToken.txt");
-      var code = tokenRepository.Load()[0];
-
-      var state = new AuthorizationState(
-        Constants.GetScopes())
-      {Callback = new Uri(NativeApplicationClient.OutOfBandCallbackUrl), RefreshToken = code};
-      appClient.RefreshToken(state);
-      return state;
-    }*/
   }
 }
 
