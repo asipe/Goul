f3831f93576ecd72a52062f7a7f4fb463084dfaa
Andy Sipe <morgan.talk@gmail.com>
2013-07-26 09:40:35 -0400

    Added the basic UpdaterTest + src

---
+++ src/Goul.Console.Core/Updater.cs
@@ -0,0 +1,5 @@
+﻿namespace Goul.Console.Core {
+  public class Updater {
+     
+  }
+}
---
+++ src/Goul.UnitTests/UpdaterTest.cs
@@ -0,0 +1,23 @@
+﻿using Goul.Console.Core;
+using Moq;
+using NUnit.Framework;
+using SupaCharge.Testing;
+
+namespace Goul.UnitTests {
+  [TestFixture]
+  public class UpdaterTest:BaseTestCase {
+    [Test]
+    public void UpdateASingleFileOnRoot() {
+      //mIsUpdateRequired.Setup(u => u.CheckDirectoryForUpdate(new[] { "file1" }, "file1")).Returns(true);
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mUpdater = new Updater();
+      mIsUpdateRequired = Mok<IsUpdateRequired>();
+    }
+
+    private Updater mUpdater;
+    private Mock<IsUpdateRequired> mIsUpdateRequired;
+  }
+}
