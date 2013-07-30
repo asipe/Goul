d7bb6ab08c6c0f05e934e15ba3a953d45f5a924e
Andy Sipe <morgan.talk@gmail.com>
2013-07-30 09:27:10 -0400

    Setup the initial msbuild integration from meldz

---
+++ scripts/minion.psm1
@@ -0,0 +1,132 @@
+function configure() {
+  $root = '.'
+  $src = Join-Path $root 'src'
+  $thirdparty = Join-Path $root 'thirdparty'
+  $debug = Join-Path $root 'debug'
+
+  return @{
+    thirdparty_dir = $thirdparty
+    src_dir = $src
+    packages_dir = Join-Path $thirdparty 'packages'
+    debug_dir = $debug
+  }
+}
+
+$config = configure
+
+function CheckLastExitCode() {
+  if ($? -eq $false) {
+    throw 'Command Failed'
+  }
+}
+
+function tryDelete($path) {
+  if (Test-Path($path)) {
+    Remove-Item $path -Verbose -Recurse
+  }
+}
+
+function help() {
+  Write-Host -ForegroundColor Green 'Minion Config'
+  write-host -ForegroundColor Green (ConvertTo-Json $config)
+}
+
+function bootstrap() {
+  .\thirdparty\nuget\nuget.exe install .\src\Goul.Nuget.Packages\common\packages.config -OutputDirectory .\thirdparty\packages\common -ExcludeVersion | Write-Host
+  .\thirdparty\nuget\nuget.exe install .\src\Goul.Nuget.Packages\net-4.0\packages.config -OutputDirectory .\thirdparty\packages\net-4.5 -ExcludeVersion | Write-Host
+}
+
+function clean() {
+  tryDelete($config.debug_dir)
+}
+
+function cleanAll() {
+  tryDelete($config.packages_dir)
+}
+
+function runUnitTestsVS() {
+  Write-Host -ForegroundColor Cyan '----------VS Unit Tests-----------'
+  .\thirdparty\packages\common\nunit.runners\tools\nunit-console.exe .\src\DocumentUploader.UnitTests\bin\debug\DocumentUploader.UnitTests.dll /nologo | Write-Host
+  CheckLastExitCode
+  Write-Host -ForegroundColor Cyan '----------------------------------'
+}
+
+function runUnitTests() {
+  Write-Host -ForegroundColor Cyan '-------Debug Unit Tests-----------'
+  .\thirdparty\packages\common\nunit.runners\tools\nunit-console.exe .\debug\net-4.5\DocumentUploader.UnitTests\DocumentUploader.UnitTests.dll /nologo | Write-Host
+  CheckLastExitCode
+  Write-Host -ForegroundColor Cyan '----------------------------------'
+}
+
+function runIntegrationTestsVS() {
+  Write-Host -ForegroundColor Cyan '------VS Integration Tests--------'
+  .\thirdparty\packages\common\nunit.runners\tools\nunit-console.exe .\src\Meldz.IntegrationTests\bin\debug\Meldz.IntegrationTests.dll /nologo | Write-Host
+  CheckLastExitCode
+  Write-Host -ForegroundColor Cyan '----------------------------------'
+}
+
+function runIntegrationTests() {
+  Write-Host -ForegroundColor Cyan '-----Debug Integration Tests------'
+  .\thirdparty\packages\common\nunit.runners\tools\nunit-console.exe .\debug\net-4.5\Meldz.IntegrationTests\Meldz.IntegrationTests.dll /nologo | Write-Host
+  CheckLastExitCode
+  Write-Host -ForegroundColor Cyan '----------------------------------'
+}
+
+function runAllTests() {
+  runUnitTests
+  runUnitTestsVS
+}
+
+function build() {
+  C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\src\Goul.Build\Goul.proj /ds /maxcpucount:4 | Write-Host
+  CheckLastExitCode
+}
+
+function setEnv() {
+  $env:PATH += ";.\thirdparty\packages\common\NUnit.Runners\tools"
+  Write-Host -ForegroundColor Green 'Path information set'
+}
+
+function cycle() {
+  build
+  runUnitTests
+}
+
+function minion {
+  param([string[]] $commands)
+
+  $ErrorActionPreference = 'Stop'
+
+  $commands | ForEach {
+    $command = $_
+    $times = Measure-Command {
+      
+      Write-Host -ForegroundColor Green "command: $command"
+      Write-Host ''
+      
+      switch ($command) {
+        'help' { help }
+        'bootstrap' { bootstrap }
+        'set.env' { setEnv }
+        'clean.all' { 
+          clean
+          cleanAll 
+        }
+        'clean' { clean }
+        'run.unit.tests.vs' { runUnitTestsVS }
+        'run.unit.tests' { runUnitTests }
+        'run.integration.tests.vs' { runIntegrationTestsVS }
+        'run.integration.tests' { runIntegrationTests }
+        'run.all.tests' { runAllTests }
+        'build' { build }
+        'cycle' { cycle }
+        default { Write-Host -ForegroundColor Red "command not known: $command" }
+      }
+    }
+    
+    Write-Host ''
+    Write-Host -ForegroundColor Green "Complete."
+  }
+}
+
+export-modulemember -function minion
---
+++ src/Common.Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Beta")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Z Bar Technologies, LLC")]
+[assembly: AssemblyCopyright("Copyright © Andy Sipe 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
