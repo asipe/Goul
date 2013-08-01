7b900b8ea48bff01a1a56f8c3a884e3090fa43ea
Andy Sipe <morgan.talk@gmail.com>
2013-08-01 14:53:24 -0400

    Added the set credentials unitTest

---
+++ src/DocumentUploader.UnitTests/SetCredentialsTest.cs
@@ -0,0 +1,27 @@
+﻿using DocumentUploader.Core;
+using DocumentUploader.Core.Command;
+using DocumentUploader.Core.Observer;
+using Moq;
+using NUnit.Framework;
+using SupaCharge.Testing;
+
+namespace DocumentUploader.UnitTests {
+  [TestFixture]
+  public class SetCredentialsTest : BaseTestCase {
+    [Test]
+    public void TestExecuteAddsOneCorrectMessageToTheObserver() {
+      mMessageObserver.Setup(o => o.AddMessages(new[] {"Credentials Set"}));
+      mSetCredentialsCommand.Execute(new[] { "setcredentials", "1", "2" });
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mMessageObserver = Mok<IMessageObserver>();
+      mSetCredentialsCommand = new SetCredentialsCommand(mMessageObserver.Object);
+    }
+
+    private Mock<IMessageObserver> mMessageObserver;
+    private ICommand mSetCredentialsCommand;
+
+  }
+}
