48a767852599b4abc1e19a1b3a39a58a4b1b8891
Andy Sipe <morgan.talk@gmail.com>
2013-08-07 08:47:01 -0400

    Added the base for the AuthorizationTests

---
+++ src/DocumentUploader.IntegrationTests/AuthorizeGDriveAccessTest.cs
@@ -0,0 +1,31 @@
+﻿using DocumentUploader.Core.App;
+using DocumentUploader.Core.Factory;
+using DocumentUploader.Core.Factory.Module;
+using DocumentUploader.Core.Observer;
+using DocumentUploader.IntegrationTests.Infrastructure;
+using DocumentUploader.IntegrationTests.Infrastructure.Modules;
+using NUnit.Framework;
+using SupaCharge.Core.IOAbstractions;
+
+namespace DocumentUploader.IntegrationTests {
+  [TestFixture]
+  public class AuthorizeGDriveAccessTest {
+    [Test]
+    public void InitTest() {
+      
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mFactory = new Factory(new DefaultModuleConfiguration(), new ITModuleConfiguration());
+      mMessageObserver = (RecordingObserver)mFactory.Build<IMessageObserver>();
+      mApp = mFactory.Build<IApp>();
+      mFile = new DotNetFile();
+    }
+
+    private DotNetFile mFile;
+    private Factory mFactory;
+    private RecordingObserver mMessageObserver;
+    private IApp mApp;
+  }
+}
---
+++ src/DocumentUploader.IntegrationTests/PreRunSetup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DocumentUploader.IntegrationTests {
+  [SetUpFixture]
+  public sealed class GlobalSetup {
+    [SetUp]
+    public void DoSetup() {
+      Console.WriteLine("");
+      Console.WriteLine("================================================================================");
+      Console.WriteLine("================================================================================");
+      Console.WriteLine("");
+      Console.WriteLine("These integration tests will fry the given Google Drive Account.");
+      Console.WriteLine("Make sure you're okay with that account being melted.");
+      Console.WriteLine("");
+      Console.WriteLine("================================================================================");
+      Console.WriteLine("================================================================================");
+      Console.WriteLine("");
+     
+      Console.WriteLine("result");
+    }
+  }
+}
