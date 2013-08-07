a56a2bbfcb13f25089894ddcee202d7a54c8026d
Andy Sipe <morgan.talk@gmail.com>
2013-08-07 15:45:13 -0400

    Added the equality base test case

---
+++ src/DocumentUploader.UnitTests/DocumentUploaderBaseTestCase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Linq;
+using KellermanSoftware.CompareNetObjects;
+using NUnit.Framework;
+
+namespace DocumentUploader.UnitTests {
+  [TestFixture]
+  public class DocumentUploaderBaseTestCase {
+     static DocumentUploaderBaseTestCase() {
+      _ObjectComparer.MaxDifferences = 10;
+    }
+
+    protected void AssertEqual(object actual, object expected) {
+      Assert.That(AreEqual(actual, expected), Is.True, _ObjectComparer.DifferencesString);
+    }
+
+    protected static bool AreEqual(object actual, object expected) {
+      if (actual is IEnumerable && expected is IEnumerable) {
+        actual = ((IEnumerable)actual).Cast<object>().ToArray();
+        expected = ((IEnumerable)expected).Cast<object>().ToArray();
+      }
+      return _ObjectComparer.Compare(actual, expected);
+    }
+
+    private static readonly CompareObjects _ObjectComparer = new CompareObjects();
+  }
+}
