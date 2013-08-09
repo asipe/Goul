414693bea11133d4b4530d73d13aa4ec2361c570
Andy Sipe <morgan.talk@gmail.com>
2013-08-09 09:25:56 -0400

    updated the upload test to talk w/the google system

--- src/DocumentUploader.IntegrationTests/UploadTest.cs
+++ src/DocumentUploader.IntegrationTests/UploadTest.cs
@@ -1,9 +1,12 @@
-﻿using DocumentUploader.Core.App;
+﻿using System.IO;
+using DocumentUploader.Core.App;
 using DocumentUploader.Core.Factory;
 using DocumentUploader.Core.Factory.Module;
+using DocumentUploader.Core.Models;
 using DocumentUploader.Core.Observer;
 using DocumentUploader.IntegrationTests.Infrastructure;
 using DocumentUploader.IntegrationTests.Infrastructure.Modules;
+using Goul.Core;
 using NUnit.Framework;
 using SupaCharge.Core.IOAbstractions;
 using SupaCharge.Testing;
@@ -13,9 +16,15 @@ namespace DocumentUploader.IntegrationTests {
   public class UploadTest:BaseTestCase {
     [Test]
     public void TestMessage() {
+      if (!File.Exists("refreshToken.txt"))
+      File.Copy("Refresh_AuthToken_Test_Use_Only.txt", "refreshToken.txt");
+
+      File.Copy("Credentials_Test_Use_Only.txt", "credentials.txt");
       mApp.Execute("upload");
+      var files = mHandler.GetFilesByTitle(mCredentials.Get(), mRefreshToken.Get());
 
       Assert.That(mMessageObserver.GetMessages(), Is.EqualTo(BA("File uploaded")));
+      Assert.That(files.Count, Is.EqualTo(0));
     }
 
     [SetUp]
@@ -24,11 +33,17 @@ namespace DocumentUploader.IntegrationTests {
       mMessageObserver = (RecordingObserver)mFactory.Build<IMessageObserver>();
       mApp = mFactory.Build<IApp>();
       mFile = new DotNetFile();
+      mRefreshToken = new RefreshTokenStore(mFile, "refreshToken.txt");
+      mCredentials = new CredentialStore(mFile, "Credentials_Test_Use_Only.txt");
+      mHandler = new GoulRequestHandler();
     }
 
     private DotNetFile mFile;
     private Factory mFactory;
     private RecordingObserver mMessageObserver;
     private IApp mApp;
+    private ICredentialStore mCredentials;
+    private IRefreshTokenStore mRefreshToken;
+    private IGoulRequestHandler mHandler;
   }
 }
--- src/Goul.Core/IGoulRequestHandler.cs
+++ src/Goul.Core/IGoulRequestHandler.cs
@@ -1,6 +1,9 @@
-﻿namespace Goul.Core {
+﻿using System.Collections.Generic;
+
+namespace Goul.Core {
   public interface IGoulRequestHandler {
     string GetAuthUrl(Credentials credentials);
     string CreateRefreshToken(Credentials credentials, string authCode);
+    List<string> GetFilesByTitle(Credentials credentials, RefreshToken refreshToken);
   }
 }
