3ca4b38c1486e1489d0982d4561bb9cf4b00f55e
Andy Sipe <morgan.talk@gmail.com>
2013-08-12 11:41:25 -0400

    Fixed the credentialsStore + tests

--- src/DocumentUploader.UnitTests/CredentialStoreTest.cs
+++ src/DocumentUploader.UnitTests/CredentialStoreTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using DocumentUploader.Core.Models;
 using Goul.Core;
 using Moq;
@@ -11,28 +10,22 @@ namespace DocumentUploader.UnitTests {
   [TestFixture]
   public class CredentialStoreTest : BaseTestCase {
     [Test]
-    public void TestGetWithRealPathGivesCorrectValues() {
-      mFile.Setup(f => f.ReadAllLines("fakePath.txt")).Returns(new[] {"1", "2"});
+    public void TestGetWithCorrectPathReturnsValues() {
+      mFile.Setup(f => f.ReadAllLines("credentials.txt")).Returns(new[] { "1", "2" });
       Assert.That(mCredStore.Get().ClientID, Is.EqualTo("1"));
       Assert.That(mCredStore.Get().ClientSecret, Is.EqualTo("2"));
     }
 
     [Test]
-    public void TestGetWithInvalidPathThrowsAnException() {
-      mFile.Setup(f => f.ReadAllLines("fakePath.txt")).Throws(new FileNotFoundException());
-      Assert.Throws(typeof(FileNotFoundException), () => mCredStore.Get());
-    }
-
-    [Test]
-    public void TestThatUpdateGrabsCorrectValues() {
-      mFile.Setup(f => f.WriteAllText("fakePath.txt", string.Format("{0}{1}{2}", "1", Environment.NewLine, "2")));
+    public void TestThatUpdateChangesTheValuesOfTheFile() {
+      mFile.Setup(f => f.WriteAllText("credentials.txt", string.Format("{0}{1}{2}", "1", Environment.NewLine, "2")));
       mCredStore.Update(new Credentials {ClientID = "1", ClientSecret = "2"});
     }
 
     [SetUp]
     public void DoSetup() {
       mFile = Mok<IFile>();
-      mCredStore = new CredentialStore(mFile.Object, "fakePath.txt");
+      mCredStore = new CredentialStore(mFile.Object, "credentials.txt");
     }
 
     private Mock<IFile> mFile;
