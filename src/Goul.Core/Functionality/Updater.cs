cec4dcdaf6da8e2bf8b3e0524f512cc74f2dac6d
msipe <morgan.talk@gmail.com>
2013-08-14 11:12:10 -0400

    Added the updater

---
+++ src/Goul.Core/Functionality/Updater.cs
@@ -0,0 +1,11 @@
+﻿using Google.Apis.Drive.v2;
+using Goul.Core.Tokens;
+
+namespace Goul.Core.Functionality {
+  public class Updater {
+    public Updater(Credentials credentials, RefreshToken refreshToken) {
+      mService = new GetDriveService().GetService(credentials, refreshToken);
+    }
+
+    private DriveService mService;
+  }
